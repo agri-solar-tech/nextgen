ec357b437c8d7c32b48c5528781b5c42bbfef5da
agri-solar-tech <hattieellis17@gmail.com>
2025-03-30 03:32:48 +0000

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,40 +1,15 @@
-<?php
-/**
- * Requires the "PHP Email Form" library
- * The "PHP Email Form" library is available only in the pro version of the template
- * The library should be uploaded to: vendor/php-email-form/php-email-form.php
- * For more info and help: https://bootstrapmade.com/php-email-form/
- */
-
-// Replace with your real receiving email address
-$receiving_email_address = 'yassineouyahya9@gmail.com';
-
-if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
-    include($php_email_form);
-} else {
-    die('Unable to load the "PHP Email Form" Library!');
-}
-
-$contact = new PHP_Email_Form;
-$contact->ajax = true;
-
-$contact->to = $receiving_email_address;
-$contact->from_name = $_POST['name'];
-$contact->from_email = $_POST['email'];
-$contact->subject = $_POST['subject'];
-
-// SMTP Configuration (Use one of the Gmail accounts)
-$contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'lexusczeck55462@gmail.com', // Change to one of the emails
-    'password' => 'poxfelikaglbhrel', // Corresponding App Password
-    'port' => '587',
-    'encryption' => 'tls'
-);
-
-$contact->add_message($_POST['name'], 'From');
-$contact->add_message($_POST['email'], 'Email');
-$contact->add_message($_POST['message'], 'Message', 10);
-
-echo $contact->send();
-?>
+<form action="https://formspree.io/f/mrbpaann" method="POST">
+    <label>
+        Your Name:
+        <input type="text" name="name" required placeholder="Your Name">
+    </label>
+    <label>
+        Your Email:
+        <input type="email" name="email" required placeholder="Your Email">
+    </label>
+    <label>
+        Your Message:
+        <textarea name="message" required placeholder="Your Message"></textarea>
+    </label>
+    <button type="submit">Send</button>
+</form>
